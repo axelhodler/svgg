7ced0a1f54c7d909da96c38545e551f723b599f4
Axel Hodler <axel@hodler.co>
2019-06-23 09:33:15 +0200

    dynamic drawing

--- draw.js
+++ draw.js
@@ -11,6 +11,20 @@ CONTENT = `
 <rect y="10" width="50" height="10" class="rect"/>
 <text y="18" class="text">OP_EQUAL</text>`
 
-FILE = `<svg viewBox="0 0 220 100" xmlns="http://www.w3.org/2000/svg">${STYLE}${CONTENT}</svg>`
+rects = []
+
+const appendRect = (text) => {
+  rect_y = rects.length * 10
+  text_y = rect_y + 8
+  rects.push(`
+<rect y="${rect_y}" width="50" height="10" class="rect"/>
+<text y="${text_y}" class="text">${text}</text>
+`)
+}
+
+appendRect("OP_6")
+appendRect("OP_EQUAL")
+contents = rects.join("")
+FILE = `<svg viewBox="0 0 220 100" xmlns="http://www.w3.org/2000/svg">${STYLE}${contents}</svg>`
 
 fs.writeFileSync('output.svg', FILE)
