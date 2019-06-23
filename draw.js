fdd6c2c4e9a221af5399600f6ebd2b769dbd2a56
Axel Hodler <axel@hodler.co>
2019-06-23 09:33:44 +0200

    remove obsolete contents

--- draw.js
+++ draw.js
@@ -5,14 +5,7 @@ STYLE = `<style>
   .rect { fill:white;stroke:black;stroke-width:1 }
 </style>`
 
-CONTENT = `
-<rect width="50" height="10" class="rect"/>
-<text y="8" class="text">OP_6</text>
-<rect y="10" width="50" height="10" class="rect"/>
-<text y="18" class="text">OP_EQUAL</text>`
-
 rects = []
-
 const appendRect = (text) => {
   rect_y = rects.length * 10
   text_y = rect_y + 8
