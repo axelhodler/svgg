06408636ead15841642fe3f88ca2459b8a4fe148
Axel Hodler <axel@hodler.co>
2019-06-23 09:37:50 +0200

    less state

--- draw.js
+++ draw.js
@@ -5,19 +5,19 @@ STYLE = `<style>
   .rect { fill:white;stroke:black;stroke-width:1 }
 </style>`
 
-rects = []
-const appendRect = (text) => {
+const appendRect = (rects, text) => {
   rect_y = rects.length * 10
   text_y = rect_y + 8
   rects.push(`
 <rect y="${rect_y}" width="50" height="10" class="rect"/>
 <text y="${text_y}" class="text">${text}</text>
 `)
+  return rects
 }
 
-appendRect("OP_6")
-appendRect("OP_EQUAL")
-contents = rects.join("")
+let contents = appendRect([], "OP_6")
+contents = appendRect(contents, "OP_EQUAL")
+contents = contents.join("")
 FILE = `<svg viewBox="0 0 220 100" xmlns="http://www.w3.org/2000/svg">${STYLE}${contents}</svg>`
 
 fs.writeFileSync('output.svg', FILE)
