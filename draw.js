fa4db4c5f7be01f8984f39ad671d3911af0e48a6
Axel Hodler <axel@hodler.co>
2019-06-23 12:03:34 +0200

    specify size of svg

--- draw.js
+++ draw.js
@@ -6,19 +6,20 @@ STYLE = `<style>
   .rect { fill:white;stroke:black;stroke-width:1 }
 </style>`
 
+WIDTH=50
 const appendRect = (text, index) => {
   rect_y = index * 10
   text_y = rect_y + 8
   return `
-<rect y="${rect_y}" width="50" height="10" class="rect"/>
+<rect y="${rect_y}" width="${WIDTH}" height="10" class="rect"/>
 <text y="${text_y}" class="text">${text}</text>
 `
 }
 
 contents = process.argv[2].split(",").map((element, index) => {
   return appendRect(element, index)
-}).join("")
+})
 
-FILE = `<svg viewBox="0 0 220 100" xmlns="http://www.w3.org/2000/svg">${STYLE}${contents}</svg>`
+FILE = `<svg width="${WIDTH}" xmlns="http://www.w3.org/2000/svg">${STYLE}${contents}</svg>`
 
 fs.writeFileSync('output.svg', FILE)
