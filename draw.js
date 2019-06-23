5bf2c5155c4252419add18c3d58f1e4429ec82c8
Axel Hodler <axel@hodler.co>
2019-06-23 09:45:26 +0200

    nicer way to draw

--- draw.js
+++ draw.js
@@ -5,19 +5,19 @@ STYLE = `<style>
   .rect { fill:white;stroke:black;stroke-width:1 }
 </style>`
 
-const appendRect = (rects, text) => {
-  rect_y = rects.length * 10
+const appendRect = (text, index) => {
+  rect_y = index * 10
   text_y = rect_y + 8
-  rects.push(`
+  return `
 <rect y="${rect_y}" width="50" height="10" class="rect"/>
 <text y="${text_y}" class="text">${text}</text>
-`)
-  return rects
+`
 }
 
-let contents = appendRect([], "OP_6")
-contents = appendRect(contents, "OP_EQUAL")
-contents = contents.join("")
+contents = ["OP_6", "OP_EQUAL"].map((element, index) => {
+  return appendRect(element, index)
+}).join("")
+
 FILE = `<svg viewBox="0 0 220 100" xmlns="http://www.w3.org/2000/svg">${STYLE}${contents}</svg>`
 
 fs.writeFileSync('output.svg', FILE)
