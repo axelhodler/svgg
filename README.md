785e183e4f70c716ac3d464b752ee00b21051d85
Axel Hodler <axel@hodler.co>
2019-06-30 10:11:34 +0200

    can draw stack state

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Usage
 
 ```
-node draw.js "OP_6,OP_EQUAL"
+node draw.js "OP_6,OP_EQUAL,OP_NOT" "STACK_1,STACK_2"
 ```
 
 # Optimization
--- draw.js
+++ draw.js
@@ -1,30 +1,45 @@
 const fs = require('fs')
 const process = require('process')
 const script_opcodes = process.argv[2].split(",")
+const stack_state = process.argv[3].split(",")
 
 STYLE = `<style>
   .text { font: 9px sans-serif; }
   .rect { fill:white;stroke:black;stroke-width:1 }
 </style>`
 
-WIDTH=50
-const rectWithText = (rect_x, text_x, rect_y, text_y, text) => {
+WIDTH=150
+COLUMN_WIDTH=50
+const rectWithText = (x_offset, rect_y, text_y, text) => {
   return `
-<rect x="${rect_x}" y="${rect_y}" width="${WIDTH}" height="10" class="rect"/>
-<text x="${text_x}" y="${text_y}" class="text">${text}</text>
+<rect x="${x_offset}" y="${rect_y}" width="${COLUMN_WIDTH}" height="10" class="rect"/>
+<text x="${x_offset}" y="${text_y}" class="text">${text}</text>
 `
 }
 
 const scriptRect = (text, index) => {
   rect_y = index * 10
   text_y = rect_y + 8
-  return rectWithText(0, 0, rect_y, text_y, text)
+  return rectWithText(0, rect_y, text_y, text)
+}
+
+const stackRect = (text, index) => {
+  rect_y = index * 10
+  text_y = rect_y + 8
+  return rectWithText(COLUMN_WIDTH * 2, rect_y, text_y, text)
 }
 
 contents = script_opcodes.map((element, index) => {
   return scriptRect(element, index)
 })
 
-FILE = `<svg width="${WIDTH}" xmlns="http://www.w3.org/2000/svg">${STYLE}${contents.join("")}</svg>`
+stack_contents = stack_state.map((element, index) => {
+  return stackRect(element, index)
+})
+
+FILE = `<svg width="${WIDTH}" xmlns="http://www.w3.org/2000/svg">
+  ${STYLE}${contents.join("")}
+  ${stack_contents.join("")}
+</svg>`
 
 fs.writeFileSync('output.svg', FILE)
